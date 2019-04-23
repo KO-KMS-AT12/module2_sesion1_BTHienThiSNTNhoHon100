a4945c99e88bce3b59a7d38f8ae2c62c0b762e18
ko <vtg0502@gmail.com>
2019-04-23 10:32:10 +0700

    hien thi snt nho hon 100

--- src/Main.java
+++ src/Main.java
@@ -12,7 +12,7 @@ public class Main {
     static boolean checkSNT(int number) {
         if (number < 2) return false;
         else {
-            for (int i = 2; i < Math.sqrt(number); i++) {
+            for (int i = 2; i <= Math.sqrt(number); i++) {
                 if (number % i == 0) return false;
             }
         }
